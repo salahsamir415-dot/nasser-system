836ba15e2b283abf79ff3771bf45be87386d1f52
salahsamir415-dot <salahsamir415@gmail.com>
2026-02-25 15:05:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
 const cacheName = 'nasser-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/1041/1041916.png'
-];
+const assets = ['./', './index.html', './manifest.json'];
 
+// حفظ ملفات البرنامج
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
+// تشغيل البرنامج من الذاكرة لو مفيش نت
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
